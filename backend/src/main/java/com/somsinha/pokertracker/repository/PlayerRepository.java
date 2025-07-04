5fab22ed11161cd69d3a44b6d9588da9912798e0
Som Sinha <somdsinha@gmail.com>
2025-07-04 23:54:31 +0100

    [new] add deleteByGame method in PlayerRepository

--- backend/src/main/java/com/somsinha/pokertracker/repository/PlayerRepository.java
+++ backend/src/main/java/com/somsinha/pokertracker/repository/PlayerRepository.java
@@ -10,4 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PlayerRepository extends JpaRepository<Player, UUID> {
 
   List<Player> findByGame(Game game);
+
+  void deleteByGame(Game game);
 }
